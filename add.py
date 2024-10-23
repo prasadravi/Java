7e5041232a42ec0047b783d86e872cbb9455b5aa
Ravi prasad <prasadravi9866@gmail.com>
2024-10-23 23:56:59 +0530

    market management

--- add.py
+++ add.py
@@ -1,3 +1,38 @@
-x=7
-x+=x
-print(x)
+dict3={'key4':3,'key5':4}
+print(dict3.get(2))
+
+dict={"name":"prasad","age":29,"dep":"DS"}
+for i in dict.keys():
+    print(i)
+for j in 
+dict.value():
+    print(j)
+for x in dict_a.items():
+    print(dict({x}))
+    print(type(x))
+
+
+l̥ 
+dict={1:'a',2:'b'}
+d1=dict.cpoy
+print(i)
+
+
+l=[1,3,4,5,8]
+l1=[]
+for i in l:
+    line=[]
+for j in range(i):
+    line .append(j)
+    l.append(line)
+    print(l)
+
+
+
+
+def greet(frist_name,last_name):
+    print('Hi {} {}'.format(frist_name,last_name))
+    greet('jhon','victor')
+    greet('sasi','kumar'
+          
+try: 
--- hello..py
+++ hello..py
@@ -1,2 +1,31 @@
-print("hello")
-print("world")
+age=23
+if(age<14):
+    print("minor")
+elif(age>14 and age<=34):
+    print("working professionals")
+else:
+    print("seinor citizen")
+
+
+
+
+strl=['a','b','c','d']
+dict={}
+for  i in strl:
+ if i not in dict.keys():
+    dict[i]=0
+dict[i]+=1
+print('aplhacon',dict)
+
+
+
+import numpy as np
+import time
+import sys
+
+s = range(1000)
+
+print(sys.getsizeof(5) * len(s))
+D = np.arange(1000)
+
+print(D.size * D.itemsize)
